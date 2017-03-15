d6c9cebc1d35de318f3882b62204a752eb75c298
loui.wang <e874314@gmail.com>
2017-03-15 16:43:19 +0800

    add db

--- app/controllers/DataController.scala
+++ app/controllers/DataController.scala
@@ -3,30 +3,52 @@ package controllers
 import java.util.UUID
 
 import controllers.response.DataResponse
-import io.swagger.annotations.Api
+import io.swagger.annotations._
 import model.{Data}
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.mvc._
 
-@Api(value = "data",
-     produces = "application/json",
-     consumes = "application/json")
+@Api(value = "data")
 class DataController extends Controller {
-  def add = Action {
-    val data = Data.add(UUID.randomUUID().toString)
-    Ok(DataResponse.packResponse(data, "add"))
+  @ApiOperation(nickname = "新增", value = "新增Data", httpMethod = "POST")
+  @ApiImplicitParams(
+    Array(
+      new ApiImplicitParam(name = "data",
+                           defaultValue = "",
+                           value = "資料",
+                           required = true,
+                           dataType = "String",
+                           paramType = "form")))
+  def add = Action { implicit request =>
+    val parameterMap = Form("data" -> text).bindFromRequest.data
+    val newData = Data.add(parameterMap.get("data"))
+    Ok(DataResponse.packResponse(newData, "add"))
   }
 
+  @ApiOperation(nickname = "刪除", value = "刪除Data", httpMethod = "DELETE")
   def remove(id: String) = Action {
     val data = Data.delete(id)
     Ok(DataResponse.packResponse(data, "remove"))
   }
 
-  def update(id: String) = Action {
+  @ApiOperation(nickname = "更新", value = "更新Data", httpMethod = "POST")
+  @ApiImplicitParams(
+    Array(
+      new ApiImplicitParam(name = "data",
+                           defaultValue = "",
+                           value = "資料",
+                           required = true,
+                           dataType = "String",
+                           paramType = "form")))
+  def update(id: String) = Action { implicit request =>
+    val parameterMap = Form("data" -> text).bindFromRequest.data
     val oldData = Data.get(id).get
-    val data = Data.update(oldData.copy(data = UUID.randomUUID().toString))
+    val data = Data.update(oldData.copy(data = parameterMap.get("data")))
     Ok(DataResponse.packResponse(data, "update"))
   }
 
+  @ApiOperation(nickname = "取得", value = "取得Data", httpMethod = "POST")
   def get = Action { implicit request =>
     val data = Data.getAll()
     Ok(DataResponse.packResponse(data, "get"))
